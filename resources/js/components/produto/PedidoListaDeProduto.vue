73b64978655d80aadda42d5cc4b858cdb8b3cf3f
RafaelcVinicius <vinicius.colde@gmail.com>
2022-03-04 07:57:52 -0300

    ajuste para mudar de branch

---
+++ resources/js/components/produto/PedidoListaDeProduto.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="lista">
+        <table>
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Código de barras</th>
+                    <th>Qtde</th>
+                    <th>Valor</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>    
+            <tbody>
+                 <tr v-for="(produto, i) in produtos" :key="i">
+                    <td v-for="(dados, i) in produto" :key="i">{{dados}}</td> 
+                </tr>  
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        produtos(){
+            return this.$store.state.produtos
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/produto/PedidoProduto.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="comp-produto">
+        <h5>Produto</h5>
+        <pedido-pesquisa-produto />
+        <pedido-lista-de-produto />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h5{
+        font-size: 25px;
+        color: rgba(0, 0, 0, 0.75);
+        font-weight: 400;
+    }
+       .comp-produto{
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        background-color: white;
+        max-width: 1280px;
+        width: 100%;
+        padding: 35px;
+        border-radius: 12px;
+        box-shadow: 0 0 10px #ccc;
+        margin-bottom: 50px;
+    }
+</style>
